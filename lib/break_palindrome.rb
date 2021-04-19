e2586e84699d2defca31174458f915843b0a3e1d
Natasha McIntyre <natashamc.cs@gmail.com>
2021-04-19 14:19:13 +0100

    Return impossible, hard coded for first test

---
+++ lib/break_palindrome.rb
@@ -0,0 +1,3 @@
+def breakPalindrome(palindromeStr)
+  'IMPOSSIBLE'
+end
--- spec/break_palindrome_spec.rb
+++ spec/break_palindrome_spec.rb
@@ -1,1 +1,7 @@
 require 'break_palindrome'
+
+describe '#breakPalindrome' do
+  it 'returns IMPOSSIBLE for single char string' do
+    expect(breakPalindrome('b')).to eq('IMPOSSIBLE')
+  end
+end
